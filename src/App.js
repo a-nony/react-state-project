cd79fad16bb7ffb3602653a78323f2c9bf8222ab
a-nony <usmanmustafa621@gmail.com>
2023-07-31 16:24:41 +0100

    react-state-project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+//Class-based component...
+class App extends Component {
+  state = {
+    person: {
+      fullName: "Mustafa Usman",
+      
+      //Dummy text bio introduction.
+      bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      imgSrc: "./images/mustafa.jpg",
+      profession: "Engineer and an aspiring Web Developer",
+    },
+    show: false,
+    intervalId: null,
+    timeElapsed: 0,
+  };
+
+  //Interval...
+  componentDidMount() {
+    const intervalId = setInterval(this.updateTimeElapsed, 1000);
+    this.setState({ intervalId });
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.intervalId);
+  }
+
+  //Time Elapsed...
+  updateTimeElapsed = () => {
+    this.setState((prevState) => ({
+      timeElapsed: prevState.timeElapsed + 1,
+    }));
+  };
+
+  toggleShow = () => {
+    this.setState((prevState) => ({
+      show: !prevState.show,
+    }));
+  };
+
+  render() {
+    const { fullName, bio, imgSrc, profession } = this.state.person;
+    const { show, timeElapsed } = this.state;
+
+    return (
+      <div style={{ background: "linear-gradient(to right, gray, skyblue)" }}>
+        <button style={{ color: "red" }} onClick={this.toggleShow}>
+          Toggle Profile
+        </button>
+        {show && (
+          <div>
+            <h2>{fullName}</h2>
+            <img
+              style={{ height: 250, width: 250, padding: 20 }}
+              src={imgSrc}
+              alt="Avatar"
+            />
+            <p style={{ color: "white" }}> Bio: {bio}</p>
+            <p style={{ color: "purple" }}>Profession: {profession}</p>
+          </div>
+        )}
+        <p>Time Elapsed: {timeElapsed} seconds</p>
+      </div>
+    );
+  }
 }
 
 export default App;
+
+// function App() {
+//   return (
+//     <div className="App">
+      
+//     </div>
+//   );
+// }
+
